da37c29ece27a23d60975f0d0b40900eaaf6299d
Gallego0 <sg9787188@gmail.com>
2023-06-14 11:17:21 +0200

    Subo cambios en las clases

--- src/BuscarNum.java
+++ src/BuscarNum.java
@@ -1,4 +1,55 @@
-
+import java.util.*;
 public class BuscarNum {
+	
+	static Scanner sc = new Scanner (System.in);
+	
+	private int [] array = {11,3,28,4,1,12,111,2,14};
 
+	/**
+	 * 
+	 * @param array
+	 */
+	public BuscarNum(int[] array) {
+		super();
+		this.array = array;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	
+	public int[] getArray() {
+		return array;
+	}
+	/**
+	 * 
+	 * @param array
+	 */
+
+	public void setArray(int[] array) {
+		this.array = array;
+	}
+
+	/**
+	 * Metodo para buscar el numero que se quiere buscar
+	 * @param array
+	 * @return
+	 */
+	public static int Comprobar (int[] array) {
+		
+		int numBuscado=0;
+		
+		System.out.println("Introduce el numero que se quiere buscar");
+		numBuscado = sc.nextInt();
+		for (int num : array) {
+			if (numBuscado == num) {
+				System.out.println("SI");
+			}else {
+				System.out.println("NO");
+			}
+		}
+		return numBuscado;
+		
+	}
+	
 }
--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,4 +1,15 @@
-
+import java.util.*;
 public class PrincipalApp {
-
+	
+	static Scanner sc = new Scanner (System.in);
+	public static void main(String [] args) {
+		
+		int [] num = {11,3,28,4,1,12,111,2,14};
+		
+		BuscarNum buscarNum = new BuscarNum(num);
+		
+		int numIntroducido = BuscarNum.Comprobar(num);
+		
+		
+	}
 }
